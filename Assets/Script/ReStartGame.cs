31f1d45dc1513c1046782e73402f7d50bbf566df
Ekalb12 <61567104+Ekalb12@users.noreply.github.com>
2023-11-08 17:41:55 +1100

    V.1
    Added UI buttons restart and quit are working

---
+++ Assets/Script/ReStartGame.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReStartGame : MonoBehaviour
+
+{
+// This to reload the scene and go back to the begining.
+    public void ResetScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
+
