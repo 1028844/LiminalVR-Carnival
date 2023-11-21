d4102c4e455256583a567a511933716125117ebc
RockMuncher7 <Gregtash4@gmail.com>
2023-11-21 18:20:13 +1100

    Target flipping animations

---
+++ Assets/TargetPosManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class TargetPosManager : MonoBehaviour
+{
+    Animator _animator;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    public void DestroyObj()
+    {
+        Destroy(gameObject);
+    }
+
+    public void PlayAnimation()
+    {
+        _animator.Play("TargetFlip");
+    }
+}
