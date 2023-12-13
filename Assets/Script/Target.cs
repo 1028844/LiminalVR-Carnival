3b42a942d1131f0e29b4ebf79de105b1bff56362
RockMuncher7 <Gregtash4@gmail.com>
2023-12-13 19:46:05 +1100

    Made targets randomly move

--- Assets/Script/Target.cs
+++ Assets/Script/Target.cs
@@ -7,6 +7,33 @@ public class Target : MonoBehaviour, ITarget
     [SerializeField] GameObject hitSoundObj;
     public int scoreReward = 1;
 
+    [SerializeField] float movingTargetBounds;
+    bool _moving = false;
+    int _direction = 1;
+    float _speed = 1;
+    float _minMoveX, _maxMoveX;
+
+    void Start()
+    {
+        _minMoveX = transform.position.x - movingTargetBounds;
+        _maxMoveX = transform.position.x + movingTargetBounds;
+
+        _direction = Random.Range(-1, 2);
+
+        RandomizeMovingTarget();
+    }
+
+    void Update()
+    {
+        if (_moving)
+        {
+            if (transform.position.x > _maxMoveX) _direction = -1;
+            if (transform.position.x < _minMoveX) _direction = 1;
+
+            transform.position = new Vector3(transform.position.x + (_direction * _speed * Time.deltaTime), transform.position.y, transform.position.z);
+        }
+    }
+
     public void Hit()
     {
         hitEvent.Invoke();
@@ -32,6 +59,12 @@ public class Target : MonoBehaviour, ITarget
     {
         RandomSpawner.badTargets--;
         DestroyTarget();
+    }
+
+    void RandomizeMovingTarget()
+    {
+        int isMoving = Random.Range(0, 2);
+        if (isMoving == 1) _moving = true;
     }
 
     void CreateHitSound()
